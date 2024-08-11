8ecb45c1f7408dc05c2e66e709d3aa8a47919aef
Flygohr <info@flygohr.com>
2024-08-11 21:50:48 +0200

    Quartz sync: Aug 11, 2024, 9:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "unapologetic.af's digital garden",
+    pageTitle: "unapologetic.af",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
