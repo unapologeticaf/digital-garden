1bc968cf3c5606516aae951997958044d8e9d3a8
Flygohr <info@flygohr.com>
2024-08-11 20:48:03 +0200

    Quartz sync: Aug 11, 2024, 8:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Made with Quartz": "https://github.com/jackyzha0/quartz",
+      "Quartz Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
