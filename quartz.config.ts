b44fc3035ac96a87e9bd693f81fe7cf9a06f6975
Flygohr <info@flygohr.com>
2024-08-11 20:53:30 +0200

    Quartz sync: Aug 11, 2024, 8:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,23 +8,21 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "unapologetic.af's digital garden",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "unapologeticaf.github.io/digital-garden/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Roboto Black",
+        body: "Roboto Mono Light",
+        code: "Roboto Mono Light",
       },
       colors: {
         lightMode: {
